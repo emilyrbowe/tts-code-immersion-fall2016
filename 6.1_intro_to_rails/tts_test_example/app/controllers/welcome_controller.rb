f41ec2b4c904f040b7b721c2efa1a16136697017
Emily Bowe <emilyrbowe@gmail.com>
2016-10-31 19:55:06 -0500

    Created first test rails application

---
+++ 6.1_intro_to_rails/tts_test_example/app/controllers/welcome_controller.rb
@@ -0,0 +1,11 @@
+class WelcomeController < ApplicationController
+  def index
+    @name = 'Emily'
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- 6.1_intro_to_rails/tts_test_example/config/routes.rb
+++ 6.1_intro_to_rails/tts_test_example/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  root 'welcome#index'
+
+  get 'homepage' => 'welcome#index'
+
+  get 'drop_us_a_line' => 'welcome#contact'
+
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
